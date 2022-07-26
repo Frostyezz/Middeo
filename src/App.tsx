1f1acfa1770de5ef0f8660e1ab7b9626a0ab9e32
Robert Moraru <robert.moraru2012@yahoo.com>
2022-07-26 12:26:41 +0300

    added web3

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import web3 from "./web3";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  console.log(web3.version);
   return (
     <div className="App">
       <header className="App-header">
