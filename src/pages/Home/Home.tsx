9c48c36aef859899747e2595aaa787adb6d33d66
Robert Moraru <robert.moraru2012@yahoo.com>
2022-08-06 18:45:51 +0300

    added homepage

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,32 @@
 import React from "react";
 import styles from "./Home.module.scss";
+import { Button } from "@chakra-ui/react";
+import { BinocularsFill, FilePlusFill } from "react-bootstrap-icons";
 
 const Home = () => {
   return (
     <div className={styles.wrapper}>
-      <div className={styles.backdrop}></div>
+      <div className={styles.hero}>
+        <div className="flex col center-y animate__animated animate__fadeInUp">
+          <h1>
+            Find your new exciting <span>project</span> or{" "}
+            <span>freelancer</span>
+          </h1>
+          <div className="flex row">
+            <Button leftIcon={<BinocularsFill />} colorScheme="gray">
+              Discover posts
+            </Button>
+            <Button leftIcon={<FilePlusFill />} colorScheme="blue">
+              Create a post
+            </Button>
+          </div>
+        </div>
+        <img
+          src="/working_girl.png"
+          alt="working girl"
+          className="animate__animated animate__fadeIn"
+        />
+      </div>
     </div>
   );
 };
